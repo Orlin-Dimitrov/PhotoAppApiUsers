dac3649187f3ee7d048c35096de1b1afd104154f
Orlin Dimitrov <orlin.dimitrov@mail.bg>
2021-09-13 15:23:07 +0300

    Implementing User Login

--- src/main/java/com/or2d/photoapp/api/users/service/UsersService.java
+++ src/main/java/com/or2d/photoapp/api/users/service/UsersService.java
@@ -1,8 +1,12 @@
 package com.or2d.photoapp.api.users.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.or2d.photoapp.api.users.shared.UserDto;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService{
 
 	UserDto createUser(UserDto userDetails);
+	
+	UserDto getUserDetailsByEmail(String email);
 }
